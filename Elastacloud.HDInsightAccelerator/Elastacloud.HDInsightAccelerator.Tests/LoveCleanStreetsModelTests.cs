e2ce2d29218b2f091df0193725036e425c9c565c
Andy Cross <andy@elastacloud.com>
2014-02-08 11:39:05 -0800

    initial version

---
+++ Elastacloud.HDInsightAccelerator/Elastacloud.HDInsightAccelerator.Tests/LoveCleanStreetsModelTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elastacloud.HDInsightAccelerator.Models;
+using Xunit;
+
+namespace Elastacloud.HDInsightAccelerator.Tests
+{
+    public class LoveCleanStreetsModelTests
+    {
+        public class TheParseMethod
+        {
+            [Fact]
+            public void ParsesValidInputLine()
+            {
+                var inputLine =
+                    "753c846c-e255-4c74-8e6a-6f01f3990678,0891623a-ef61-45a1-ae76-23c84beac1c1,52.67547767,-1.162027678,14/11/2013 13:24,There is a lot of litter in the garden to the side of the off licence on Astill Lodge Road on the corner of Kingsbridge Road. This is a constant nuisance. It then blows along the road into private gardens.,14/11/2013 13:24,Cleared,,814,Rubbish - Dumped,There is a lot of litter in the garden to the side of the off licence on Astill Lodge Road on the corner of Kingsbridge Road. This is a constant nuisance. It then blows along the road into private gardens.,Astill Lodge Road Leicester LE4 1 Leicester,Beaumont,Leys Ward,http://api.mediaklik.com/images/0891623a-ef61-45a1-ae76-23c84beac1c1";
+
+                var result = LoveCleanStreetsModel.Parse(inputLine);
+
+                Assert.Equal("http://api.mediaklik.com/images/0891623a-ef61-45a1-ae76-23c84beac1c1", result.ImageUrl);
+            }
+            [Fact]
+            public void ParsesValidInputLineAddressWithCommas()
+            {
+                var inputLine =
+                    "753c846c-e255-4c74-8e6a-6f01f3990678,0891623a-ef61-45a1-ae76-23c84beac1c1,52.67547767,-1.162027678,14/11/2013 13:24,There is a lot of litter in the garden to the side of the off licence on Astill Lodge Road on the corner of Kingsbridge Road. This is a constant nuisance. It then blows along the road into private gardens.,14/11/2013 13:24,Cleared,,814,Rubbish - Dumped,There is a lot of litter in the garden to the side of the off licence on Astill Lodge Road on the corner of Kingsbridge Road. This is a constant nuisance. It then blows along the road into private gardens.,\"Astill Lodge Road, Leicester, LE4 1 Leicester\",Beaumont,Leys Ward,http://api.mediaklik.com/images/0891623a-ef61-45a1-ae76-23c84beac1c1";
+
+                var result = LoveCleanStreetsModel.Parse(inputLine);
+
+                Assert.Equal("http://api.mediaklik.com/images/0891623a-ef61-45a1-ae76-23c84beac1c1", result.ImageUrl);
+            }
+            [Fact]
+            public void ParsesValidInputLineAddressWithCommas_AddressTrimsQuotes()
+            {
+                var inputLine =
+                    "753c846c-e255-4c74-8e6a-6f01f3990678,0891623a-ef61-45a1-ae76-23c84beac1c1,52.67547767,-1.162027678,14/11/2013 13:24,There is a lot of litter in the garden to the side of the off licence on Astill Lodge Road on the corner of Kingsbridge Road. This is a constant nuisance. It then blows along the road into private gardens.,14/11/2013 13:24,Cleared,,814,Rubbish - Dumped,There is a lot of litter in the garden to the side of the off licence on Astill Lodge Road on the corner of Kingsbridge Road. This is a constant nuisance. It then blows along the road into private gardens.,\"Astill Lodge Road, Leicester, LE4 1 Leicester\",Beaumont,Leys Ward,http://api.mediaklik.com/images/0891623a-ef61-45a1-ae76-23c84beac1c1";
+
+                var result = LoveCleanStreetsModel.Parse(inputLine);
+
+                Assert.Equal("Astill Lodge Road, Leicester, LE4 1 Leicester", result.FormattedAddress);
+            }
+        }
+    }
+}
---
+++ Elastacloud.HDInsightAccelerator/Elastacloud.HDInsightAccelerator/Models/LoveCleanStreetsModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Elastacloud.HDInsightAccelerator.Models
+{
+    public class LoveCleanStreetsModel
+    {
+        public string ReportId { get; set; }
+        public string ReportItemId { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string ReportItemDateTime { get; set; }
+        public string ItemNotes { get; set; }
+        public string ReportDateTime { get; set; }
+        public string Completed { get; set; }
+        public string CompletedDate { get; set; }
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string ReportNotes { get; set; }
+        public string FormattedAddress { get; set; }
+        public string LA { get; set; }
+        public string Ward { get; set; }
+        public string ImageUrl { get; set; }
+
+        public static List<string> SplitCsv(string input)
+        {
+            var csvSplit = new Regex("(?:^|,)(\"(?:[^\"]+|\"\")*\"|[^,]*)", RegexOptions.Compiled);
+
+            return (from Match match in csvSplit.Matches(input) select match.Value.TrimStart(',')).ToList();
+        }
+
+        public static LoveCleanStreetsModel Parse(string inputLine)
+        {
+            var splits = SplitCsv(inputLine);
+
+            if (splits.Count() > 16)
+                return null;
+
+            var parsed = new LoveCleanStreetsModel()
+            {
+                ReportId = splits[0],
+                ReportItemId = splits[1],
+                Latitude = splits[2],
+                Longitude = splits[3],
+                ReportItemDateTime = splits[4],
+                ItemNotes = splits[5],
+                ReportDateTime = splits[6],
+                Completed = splits[7],
+                CompletedDate = splits[8],
+                CategoryId = splits[9],
+                CategoryName = splits[10],
+                ReportNotes = splits[11],
+                FormattedAddress = splits[12].Trim('\"'),
+                LA = splits[13],
+                Ward = splits[14],
+                ImageUrl = splits[15]
+            };
+
+            return parsed;
+        }
+    }
+}
